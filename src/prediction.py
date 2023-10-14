two new features/scripts: model_selection & recommendation_buy; new evaluation metric for sign predictions: negative predictive value (to rate models w.r.t. short positions); fix bug in price predictions for forecast

src/prediction.py
@@ -9,7 +9,9 @@ from utils.evaluation import compute_prediction_performances
 from utils.file_handling import write_csv_results, write_frontend_data
 
 
-def generate_predictions(model_name: str, mts: MultipleTimeSeries) -> tuple[dict, dict]:
+def generate_predictions(
+    model_name: str, mts: MultipleTimeSeries, forecast: bool = False
+) -> tuple[dict, dict]:
     if model_name in ["arima", "prod_arima"]:
         from models.base import fit_predict_arima
 
@@ -48,7 +50,10 @@ def generate_predictions(model_name: str, mts: MultipleTimeSeries) -> tuple[dict
     returns_predicted, prices_predicted = {}, {}
     for name, pred in y_preds.items():
         prices_predicted[name] = []
-        price = mts.close_prices[name][-1 - len(mts.y_test)]
+        if forecast:
+            price = mts.close_prices[name][-1]
+        else:
+            price = mts.close_prices[name][-1 - len(mts.y_test)]
         returns_predicted[name] = list(mts.get_returns_from_features(np.array(pred)))
         for r in returns_predicted[name]:
             price *= r

src/utils/evaluation.py
@@ -83,7 +83,15 @@ def evaluate_sign_predictions(gt: np.array, pr: np.array) -> dict:
     precision, recall, f1_score, _ = precision_recall_fscore_support(
         gt, pr, average="binary"
     )
-    metrics = dict(zip(["Precision", "Recall", "F1"], [precision, recall, f1_score]))
+    negative_predictive_value, _, _, _ = precision_recall_fscore_support(
+        1 - gt, 1 - pr, average="binary"
+    )
+    metrics = dict(
+        zip(
+            ["Precision", "Recall", "F1", "NPV"],
+            [precision, recall, f1_score, negative_predictive_value],
+        )
+    )
     return metrics
 
 